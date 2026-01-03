b80efbc408ec4ecee089644faccaba6147ae874b
RSK Code <contact.rskcode@gmail.com>
2026-01-03 09:07:43 +0530

    add Loader #7

---
+++ src/component/loader/LoaderMinimal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoaderMinimal = () => {
+    return (
+        <div className="flex items-center justify-center h-[50vh]">
+            <div className="w-12 h-12 border-4 border-blue-500 border-solid rounded-full animate-spin border-t-transparent"></div>
+        </div>
+    );
+};
+
+export default LoaderMinimal;
